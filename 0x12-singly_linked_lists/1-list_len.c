bc5b303779e106d21c082c60d13da8e22216ad09
DrCrasha <chiccomsane@gmail.com>
2023-11-16 05:03:10 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len -lenght of the list
+ * @h: pointer to the first node
+ * Return: number of nodes in te list
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+
+	for (i = 1; h->next != NULL; i++)
+		h = h->next;
+	return (i);
+}
